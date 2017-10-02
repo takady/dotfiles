fff5945aca18943bc4af7798ca1b5ede6c20042e
Yuichi Takada <takadyuichi@gmail.com>
2017-10-02 13:33:21 +0900

    Tweak

--- bootstrap_ln.sh
+++ bootstrap_ln.sh
@@ -4,18 +4,9 @@ set -e
 cd ~/src/github.com/takady
 git clone https://github.com/takady/.emacs.d
 git clone https://github.com/takady/oh-my-zsh
-git clone https://github.com/powerline/powerline
 
 cd ~
 ln -sf ~/src/github.com/takady/dotfiles/.zshrc
 ln -sf ~/src/github.com/takady/dotfiles/.tmux.conf
 ln -sf ~/src/github.com/takady/.emacs.d
 ln -sf ~/src/github.com/takady/oh-my-zsh .oh-my-zsh
-
-mkdir ~/.config
-cd ~/.config
-ln -sf ~/src/github.com/takady/dotfiles/.config/powerline
-
-cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-ln -sf ~/src/github.com/takady/dotfiles/Preferences.sublime-settings
-ln -sf ~/src/github.com/takady/dotfiles/osx.sublime-keymap Default\ \(OSX\).sublime-keymap
